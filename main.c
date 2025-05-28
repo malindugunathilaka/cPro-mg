edd9b41e333522df768f378db45c0fb64130cf18
malindugunathilaka <Malindugunathilaka5@gmail.com>
2025-05-28 22:43:51 +0530

    Initial commit

---
+++ main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void geet(){
+ printf("welcaome to c programming ! \n");
+}
+
+
+void nameCreater(int num){
+  printf("Enterd your name is %d \n",num);
+};
+
+
+int square(int n){
+    return n*n;
+}
+
+
+void enterUserdetails(int number,int age){
+
+  printf("Enter your number : \n");
+  scanf("%d",&number);
+  printf("Enter your age : \n");
+  scanf("%d",&age);
+  printf("HEY YOUR NUMBER IS %d \n YOUR AGE IS %d \n",number,age);
+}
+
+
+
+int Big(int a,int b){
+  if(a>b){
+    printf("big values is %d",a);
+  }else{
+  printf("big value is %d",b);}
+}
+
+
+
+int oddoreven(){
+  return n % 2 == 0;
+}
+
+
+int main()
+{
+
+    return 0;
+}
